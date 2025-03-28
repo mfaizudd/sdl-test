bc8704e343f88cf4b37c90fea2a465008e2eaa5b
Muhammad Faizud Daroin <m@faizud.net>
2025-03-28 17:01:35 +0700

    fix: optimize surface

--- src/main.cpp
+++ src/main.cpp
@@ -116,13 +116,22 @@ bool loadMedia() {
 }
 
 SDL_Surface *loadSurface(std::string path) {
+  SDL_Surface *optimizedSurface = NULL;
   SDL_Surface *loadedSurface = SDL_LoadBMP(path.c_str());
   if (loadedSurface == NULL) {
     SDL_Log("Unable to load image %s. SDL Error: %s\n", path.c_str(),
             SDL_GetError());
+  } else {
+    optimizedSurface =
+        SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+    if (optimizedSurface == NULL) {
+      SDL_Log("Unable to optimize image %s. SDL Error: %s\n", path.c_str(),
+              SDL_GetError());
+    }
+    SDL_FreeSurface(loadedSurface);
   }
 
-  return loadedSurface;
+  return optimizedSurface;
 }
 
 void close() {
